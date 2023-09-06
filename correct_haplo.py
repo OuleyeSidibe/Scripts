ccc6aada90c538ca1ea39a32ee7286602da32334
ouleye <ouleye.sidibe@inrae.fr>
2023-09-06 19:03:51 +0200

    script_done

--- correct_haplo.py
+++ correct_haplo.py
@@ -5,18 +5,18 @@ from argparse import ArgumentParser
 import sys
 import fnmatch
 import glob
+import numpy as np
 
 
-
-#################################################################################
+######################################################################################################################
 # ## Infos 
 
 """Script pour confirmer les haplotypes correctement reconstruits par DESMAN"""
 
 #Lancement du script
-# python3 correct_haplo.py -i directory -f freq_allellic -g gene-list.txt
+# python3 correct_haplo.py -i directory -f freq_allellic -g gene-list.txt - r "number_replicate" -s "seuil_replicate"
 
-#################################################################################
+#####################################################################################################################
 
 
 
@@ -27,11 +27,13 @@ def config_parameters():
     parser.add_argument("-i", "--input", dest="directory", help="Desman directory")
     parser.add_argument("-f", "--freq_allelic", dest="freq_dir", help="freq allelic OSI direcetory")
     parser.add_argument("-g", "--gene_name", dest="gene_name", help="list of gene names")
-    parser.add_argument("-r", "--repic", dest="nb_repic", default="", help="number of replicats use in DESMAN")
+    parser.add_argument("-r", "--repic", dest="nb_replicate", default="", help="number of replicats use in DESMAN")
+    parser.add_argument("-s", "--seuil", dest="seuil_replicate", help="minimal seuil of haplotype redondancy in replicate")
+
     options = parser.parse_args()
-    if len(sys.argv)< 4:
+    if len(sys.argv)< 5:
         sys.exit("warning : wrong number of arguments!!")
-    return options.directory, options.freq_dir, options.gene_name, options.nb_repic
+    return options.directory, options.freq_dir, options.gene_name, options.nb_replicate, options.seuil_replicate
 
 
 
@@ -43,142 +45,168 @@ def optimal_variant(haplo_file):
     return numb_OptVar
 
 
+
+# recupérer les variants majoritaires sous forme de liste et les positions variables de chaque fichier haplo
+def maj_variants(haplo_file_path):
+
+    read_file=pd.read_csv(haplo_file_path, index_col=[0])
+    var_pos = read_file["Position"]
+    read_file= read_file.drop(columns=["Position"])
+    read_file.columns= range(1, len(read_file.columns)+1)
+    variants=[]
+    for col in read_file.columns:
+        variant= read_file[col].tolist()
+        variants.append(variant)
+    return variants, var_pos
+
+
+
+
+
 def main():
 
-    directory, freq_dir, gene_name, nb_repic= config_parameters()
+    directory, freq_dir, gene_name, nb_replicate, seuil_replicate= config_parameters()
     
     ##Lire la liste des noms de gènes
     gene_names = pd.read_csv(gene_name, index_col=[0], header=None)
     
     ##Pour chaque gène de la liste des génes analysés par DESMAN
+    nb_gene=0
     for gene in gene_names.index:
         print(gene)
         ##DESMAN gene directory
         
-        dir_gene = os.path.join(directory + "/DESMAN_" + gene)
+        gene_dir = os.path.join(directory + "/DESMAN_" + gene)
         # Fichier de sortie
-        out_path = os.path.join(dir_gene, "correct_haplotype.csv")
+        out_path = os.path.join(gene_dir, "correct_haplotype.csv")
 
         out_file = open(out_path, "w")
 
 
         ##Lister les fichier du répertoire
-        for haplo_file in os.listdir(dir_gene):
-            nb_var=0
-            # print(haplo_file)
-            
+        nb_haplo=0
+        for haplo_file in os.listdir(gene_dir):
+           
             #Si le pattern du fichier est haplo_*
             if fnmatch.fnmatch(haplo_file, "haplo_*"):
 
-                ##eliminer les fichiers haplo avec un nombre de variant incomplet
-                read_haplo =pd.read_csv(f"{dir_gene}/{haplo_file}", index_col=[0])
-                haplo = read_haplo.iloc[:, 1:]
-                length_haplo = len(haplo.columns)
-                
+                haplo_file_path = os.path.join(gene_dir, haplo_file)
 
-                # numb_OptVar= int(haplo_file.split("_")[1].replace("Variant.csv",''))
-                # print(type(numb_OptVar))
-                # print(type(nb_repic))
-                # nb_varDES = int(numb_OptVar) * int(nb_repic)
-                # print(nb_varDES)
-                # print(optimal_variant(haplo_file))
-                # print(length_haplo)
-                print(type(nb_repic))
-                print(optimal_variant(haplo_file)*int(nb_repic))
-                if (optimal_variant(haplo_file)*int(nb_repic)) == length_haplo:
-                    print(f"{haplo_file} bon nombre de variant")
-                
-                
-                    #vecteur vide pour stocker le nombre de bonnes pos var par haplotypes
-                    nb_Haplo=0 
-
-                    ##Recupérer les positions variables dans une liste
-                    var_pos = read_haplo.Position
-                    ## Recupérer le nombre de position variable du géne
-                    nb_varpos = len(var_pos)
-                    
-                    ##Parcourir chaque position variable du gène
-                    for i in range (0,nb_varpos):
-                        ##Pour chaque position position variable
-                        var_pos = read_haplo.iloc[i,0]
-                        ##Recupérer les bases uniques pour la position 
-                        df_unique = read_haplo.iloc[i,1:].unique()
-
-                        ##Si le nombre de bases unique de la position est inférieur ou égale à 2
-                        if len(df_unique) <= 2:
-
-                            ##Lister les chemin des sous repertoires du dossier freq
-                            freq_dossier = glob.glob(f"{freq_dir}/*/*.freq")
-                            
-                            ##Créer un df vide 
-                            posVar_file=pd.DataFrame()
-
-                            ## Pour chaque sous repertoire
-                            for freq in freq_dossier:
-                                ##Recupérer le nom d'échantillon
-                                sample_name = os.path.basename(os.path.dirname(freq))
-                                ##Définir le pattern du fichier
-                                pattern = (f"{freq_dir}/{sample_name}/{gene}.freq")
-                                #Si le fichier.freq du géne est présent dans l'échantillon
-                                if freq == pattern :
-                                    ##Essaye d'ouvrir le fichier pour lecture
-                                    try:
-                                        read_freqfile = pd.read_csv(pattern, sep="\t", index_col=[0])
-                                        ##Recupérer les comptages de la position variable
-                                        read_varpos = read_freqfile.loc[var_pos,:]
-                                        ##Concatner tous les comptages de la position var suivant les différents échantillons où le géne est retrouvé
-                                        posVar_file = pd.concat([posVar_file, read_varpos], axis=1)
-                                        ## Si la position variable est absente
-                                    except:
-                                        #Sinon imprimer :
-                                        print(f"{var_pos} not found in {sample_name}")
-                                
-                            ##Pour plus d'une position variable essayer:
-                            try:
-                                ##Somme des comptages de la premiére base
-                                varpos_1 = sum(posVar_file.loc[df_unique[0],:])
-                                ##Somme des comptages de la seconde base
-                                varpos_2 = sum(posVar_file.loc[df_unique[1],:])
-                                
-                                ##Si la somme des comptages des deux bases uniques est différent de 0 pour la mm position
-                                if varpos_1 != 0 and varpos_2 != 0:
-                                    ##L'haplotype est jugé correcte pour cette position
-                                    nb_Haplo += 1
-                                ## Si la somme des comptages d'une des bases unique est =0
-                                elif varpos_1 == 0 or varpos_2 == 0:
-                                    ##L'haplotype est jugé incorrecte pour cette position
-                                    nb_Haplo += 0
-                                
-                                
-                            except:
-                                varpos_1 = sum(posVar_file.loc[df_unique[0],:])
-                                # print(pos_1)
-                                if varpos_1 != 0 :
-                                    ##L'haplotype est jugé correcte pour cette position
-                                    nb_Haplo += 1
-
-                                elif varpos_1 == 0 :
-                                    ##L'haplotype est jugé incorrecte pour cette position
-                                    nb_Haplo += 0
-                        ## Sinon
-                        else:
-                            ##L'haplotype est jugé incorrecte pour cette position
-                            nb_Haplo += 0
-
-                    ##Si le nombre d'haplotye jugé correcte est égale au nombre de position 
-                    if nb_Haplo == nb_varpos:
-                        #L'haplotype est jugé corecte pour toutes les positions
-                        out_file.write(f"{haplo_file} \n")    
-                    
-                    ##Position variable suivante
-                    nb_var +=1
-                else:
-                    print(f"{haplo_file} mauvais nombre de variant")
-                
-
-
-        
+                variants , var_pos = maj_variants(haplo_file_path)
             
+
+                """ Step_1 : ne pas analyser les fichiers de variants incomplets """
+
+                if (optimal_variant(haplo_file)*int(nb_replicate)) == len(variants):
+                    # print(f"{haplo_file} bon nombre de variant")
+                    # print(len(variants))
+
+
+                    """ Step_2 : Garder les haplotypes présents au moins dans 80% des réplicats  """
+                
+                    variants_not_dup = [list(x) for x in set(tuple(x) for x in variants)]
+                
+                    variant_file=pd.DataFrame(var_pos)
+
+                    for variant in variants_not_dup:
+                        # print(variant)
+                        nb_dup=0
+                        for var in variants:
+                            # print(var)
+                            if variant == var:
+                                nb_dup+=1
+                        
+                        if nb_dup >= int(seuil_replicate):
+                            # variant = pd.Series(variant, index= var_pos)
+                            variant_file =pd.concat([variant_file, pd.Series(variant)], axis=1)
+                            variant_file.to_csv(f"{gene_dir}/{haplo_file}")
+                            
+                    # print(variant_file)
+                    nb_haplo+=1
+
+                
+                else :
+                    os.remove(f"{gene_dir}/{haplo_file}")
+
+        """ Step_3 : créer un fichier des variants uniques et majoritaires  """
+
+        final_variant=pd.DataFrame()
+        
+        nb_var=0
+        for i in range(1, nb_haplo+1):
+            j= i+1
+
+            try:
+                variants_1, var_pos_1 = maj_variants(f"{gene_dir}/haplo_{i}Variant.csv")
+                variants_2, var_pos_2 = maj_variants(f"{gene_dir}/haplo_{j}Variant.csv")
+
+                commun_variant=[]
+                for var in variants_1:
+                    if var in variants_2:
+                        commun_variant.append(var)
+                        
+                    if commun_variant == variants_1:
+                        nb_var+=1
+
+
+                    else:
+                        break
+
+            except FileNotFoundError: 
+                # print(f"file haplo_{j} not find")
+                pass
+            
+
+        if nb_var == 0:
+            final_variant = pd.concat([final_variant, pd.Series(var)], axis=1)
+
+        elif nb_var != 0:       
+            for var in variants_2:
+                final_variant = pd.concat([final_variant, pd.Series(var)], axis=1) 
+
+        final_variant.to_csv(f"{gene_dir}/final_variant.csv")
+        print(final_variant)
+
+
+        """ Step_4: vérifier si les variants majoritaires sont correctements reconstruits  """
+
+        # Ouvrir les tables de comptages
+        freq_repository = glob.glob(f"{freq_dir}/*")
+        final_variant.columns = range(1, len(final_variant.columns)+1)
+
+        nb_var=0
+        for col in final_variant.columns:
+
+            nb_base=0
+            index_var=0
+            for base in final_variant[col].tolist():
+
+                for freq in freq_repository:
+                    try:
+                        comptage = pd.read_csv(f"{freq}/{gene}.freq", sep="\t", index_col=[0])
+                        if var_pos[index_var] in comptage.index:
+                            if comptage.loc[var_pos[index_var],base] != 0:
+                                nb_base+=1
+                                index_var+=1
+                                break
+                            
+                    except FileNotFoundError :
+                        print(f"{freq}/{gene}.freq not exist or position not exit in file")
+
+            if nb_base == len(final_variant.index):
+                nb_var+=1
+               
+
+            else :
+                final_variant=final_variant.drop(columns=col, axis=1)
+
+        if nb_var == len(final_variant.columns):
+            # print("All variant correct")
+            final_variant.to_csv(f"{gene_dir}/final_variant.csv")
+            print(final_variant)
+            nb_gene+=1
+
+
 print("Process done succesfully!!")
 
 if __name__ == "__main__":
