22830d240d50a234a9329a865992590d2a5e4813
ouleye <ouleye.sidibe@inrae.fr>
2023-09-06 22:41:42 +0200

    script find_replicate

--- script_analyseDESMAN.sh
+++ script_analyseDESMAN.sh
@@ -18,7 +18,7 @@
 
 conda activate DESMAN_python
 
-directory="/home/osidibe/work/fluGenAvi_fugace/Analyse_postDESMAN_P2/DESMAN_iterAddPost
+directory="/home/osidibe/work/fluGenAvi_fugace/Analyse_postDESMAN_P2/DESMAN_iterAddPost"
 gene_name="/home/osidibe/work/fluGenAvi_fugace/Analyse_postDESMAN_P2/genes_list.txt"
 freq_dir="/home/osidibe/work/fluGenAvi_fugace/freq_allelic_OSI"
 scripts="/home/osidibe/work/fluGenAvi_fugace/scripts"
@@ -26,10 +26,13 @@ scripts="/home/osidibe/work/fluGenAvi_fugace/scripts"
 
 ## nombre de variant analysé avec DESMAN
 nb_var=5  
+nb_replicate=10
+threshold_replicate=8
 
 
 #Script python pour concatener les haplotypes DES des différents réplicats pour chacun des variant
 python3 $scripts/concate_haplo.py -i $directory -g $gene_name -v $nb_var
-python3 $scripts/correct_haplo.py -i $directory -f $freq_dir -g $gene_name
+python3 $scripts/correct_haplo.py -i $directory -f $freq_dir -g $gene_name -r $nb_replicate -s $threshold_replicate
+
 
 conda deactivate
